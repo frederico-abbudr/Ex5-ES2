c1524cfcf15b311cacb4940a80ba40c40d92394f
frederico-abbudr <frederico.abbud.reis@gmail.com>
2023-11-24 22:20:32 -0300

    first commit

---
+++ ToDoList.py
@@ -0,0 +1,51 @@
+class ToDoList:
+    def __init__(self):
+        self.tarefas = []
+
+    def adicionar_tarefa(self, tarefa):
+        self.tarefas.append(tarefa)
+        print(f'Tarefa "{tarefa}" adicionada com sucesso!')
+
+    def listar_tarefas(self):
+        if not self.tarefas:
+            print('Nenhuma tarefa na lista.')
+        else:
+            print('Tarefas:')
+            for i, tarefa in enumerate(self.tarefas, 1):
+                print(f'{i}. {tarefa}')
+
+    def remover_tarefa(self, indice):
+        try:
+            tarefa_removida = self.tarefas.pop(indice - 1)
+            print(f'Tarefa "{tarefa_removida}" removida com sucesso!')
+        except IndexError:
+            print('Índice inválido. A tarefa não foi removida.')
+
+def main():
+    lista_de_tarefas = ToDoList()
+
+    while True:
+        print('\n===== Gerenciador de Tarefas =====')
+        print('1. Adicionar Tarefa')
+        print('2. Listar Tarefas')
+        print('3. Remover Tarefa')
+        print('4. Sair')
+
+        escolha = input('Escolha uma opção (1/2/3/4): ')
+
+        if escolha == '1':
+            tarefa = input('Digite a tarefa: ')
+            lista_de_tarefas.adicionar_tarefa(tarefa)
+        elif escolha == '2':
+            lista_de_tarefas.listar_tarefas()
+        elif escolha == '3':
+            indice = int(input('Digite o índice da tarefa a ser removida: '))
+            lista_de_tarefas.remover_tarefa(indice)
+        elif escolha == '4':
+            print('Saindo do Gerenciador de Tarefas. Até logo!')
+            break
+        else:
+            print('Opção inválida. Por favor, escolha uma opção válida.')
+
+if __name__ == "__main__":
+    main()
---
+++ ToDoList_test.py
@@ -0,0 +1,47 @@
+import unittest
+
+class TestToDoList(unittest.TestCase):
+
+    def setUp(self):
+        self.lista_de_tarefas = ToDoList()
+
+    def test_adicionar_tarefa(self):
+        self.lista_de_tarefas.adicionar_tarefa("Estudar Python")
+        self.assertEqual(self.lista_de_tarefas.tarefas, ["Estudar Python"])
+
+    def test_listar_tarefas(self):
+        self.lista_de_tarefas.adicionar_tarefa("Fazer compras")
+        self.lista_de_tarefas.adicionar_tarefa("Passear com o cachorro")
+        
+        import io
+        from contextlib import redirect_stdout
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.lista_de_tarefas.listar_tarefas()
+            output = buf.getvalue().strip()
+
+        self.assertIn("1. Fazer compras", output)
+        self.assertIn("2. Passear com o cachorro", output)
+
+    def test_remover_tarefa(self):
+        self.lista_de_tarefas.adicionar_tarefa("Ler livro")
+        self.lista_de_tarefas.adicionar_tarefa("Fazer exercícios")
+        
+        self.lista_de_tarefas.remover_tarefa(1)
+        self.assertEqual(self.lista_de_tarefas.tarefas, ["Fazer exercícios"])
+
+    def test_remover_tarefa_com_indice_invalido(self):
+        self.lista_de_tarefas.adicionar_tarefa("Estudar música")
+        
+        import io
+        from contextlib import redirect_stdout
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.lista_de_tarefas.remover_tarefa(2)
+            output = buf.getvalue().strip()
+
+        self.assertIn("Índice inválido. A tarefa não foi removida.", output)
+        self.assertEqual(self.lista_de_tarefas.tarefas, ["Estudar música"])
+
+if __name__ == '__main__':
+    unittest.main()
